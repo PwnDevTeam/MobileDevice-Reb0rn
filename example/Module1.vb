45eaf634623d81b2c1e3e22e825f3207a0b31251
Fallensn0w <thimcederlund@hotmail.com>
2010-08-26 19:25:44 +0200

    example program added

---
+++ example/Module1.vb
@@ -0,0 +1,13 @@
+﻿
+Module Module1
+
+    Sub Main()
+
+        Dim dev As New MobileDevice(MobileDevice.DeviceMode.Normal)
+
+        Console.WriteLine("Device is connected: " & dev.Connect)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
